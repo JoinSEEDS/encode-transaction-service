9f70ddf05198be92b561a6cd2aa21543a25b7884
NIK <nheger@gmail.com>
2022-01-13 21:26:48 +0800

    Revert "back to 3600 seconds..."
    This reverts commit 145bb787d9cd7da739ced4995eee109ab79c2262.

--- buildTransaction.js
+++ buildTransaction.js
@@ -60,7 +60,7 @@ async function buildTransaction(actions, isTestnet = false) {
     const head_block = await rpc_active.get_block(info.last_irreversible_block_num);
     const chainId = info.chain_id;
     // set to an hour from now.
-    const expiration = Serialize.timePointSecToDate(Serialize.dateToTimePointSec(head_block.timestamp) * 3600)
+    const expiration = Serialize.timePointSecToDate(Serialize.dateToTimePointSec(head_block.timestamp) + 72 * 3600)
     const transaction = {
         expiration,
         ref_block_num: head_block.block_num & 0xffff, // 
