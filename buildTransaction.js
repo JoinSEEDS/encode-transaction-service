794bf818d586697f574a38a7ecaa15c298978a9c
NIK <nheger@gmail.com>
2022-12-08 17:00:02 +0800

    create actions based QR codes

--- buildTransaction.js
+++ buildTransaction.js
@@ -57,23 +57,23 @@ async function buildTransaction(actions, isTestnet = false) {
     const rpc_active = isTestnet ? rpc_testnet : rpc
     const opts_active = isTestnet ? opts_testnet : opts
     const info = await rpc_active.get_info();
-    const head_block = await rpc_active.get_block(info.last_irreversible_block_num);
+    // const head_block = await rpc_active.get_block(info.last_irreversible_block_num);
     const chainId = info.chain_id;
-    // set to an hour from now.
-    const expiration = Serialize.timePointSecToDate(Serialize.dateToTimePointSec(head_block.timestamp) + 3600)
-    const transaction = {
-        expiration,
-        ref_block_num: head_block.block_num & 0xffff, // 
-        ref_block_prefix: head_block.ref_block_prefix,
-        max_net_usage_words: 0,
-        delay_sec: 0,
-        context_free_actions: [],
-        actions: actions,
-        transaction_extensions: [],
-        signatures: [],
-        context_free_data: []
-    };
-    const request = await SigningRequest.create({ transaction, chainId }, opts_active);
+    // set to an hour from now. this is the max. expiry date. 
+    // const expiration = Serialize.timePointSecToDate(Serialize.dateToTimePointSec(head_block.timestamp) + 3600)
+    // const transaction = {
+    //     expiration,
+    //     ref_block_num: head_block.block_num & 0xffff, // 
+    //     ref_block_prefix: head_block.ref_block_prefix,
+    //     max_net_usage_words: 0,
+    //     delay_sec: 0,
+    //     context_free_actions: [],
+    //     actions: actions,
+    //     transaction_extensions: [],
+    //     signatures: [],
+    //     context_free_data: []
+    // };
+    const request = await SigningRequest.create({ actions, chainId }, opts_active);
     const uri = request.encode();
     return uri
 }
